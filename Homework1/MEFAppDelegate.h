62b6779d343ec3828b35e7a55b99a69c870e9aef
Matthew Fife <mefife@matthews-imac.home>
2014-06-29 21:16:57 -0700

    Initial Commit

---
+++ Homework1/MEFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MEFAppDelegate.h
+//  Homework1
+//
+//  Created by Matthew Fife on 6/29/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MEFAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Homework1/MEFAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  MEFAppDelegate.m
+//  Homework1
+//
+//  Created by Matthew Fife on 6/29/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "MEFAppDelegate.h"
+
+@implementation MEFAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
