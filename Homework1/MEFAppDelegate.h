8c2ec0de2e322b25e5e3df9db55390bb44cb4f42
mefife <mefife@matthews-imac.home>
2014-07-06 20:20:39 -0700

    Latest Changes
    Problems with formatter, but everything else appears to work.

--- Homework1/MEFAppDelegate.h
+++ Homework1/MEFAppDelegate.h
@@ -8,8 +8,24 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface MEFAppDelegate : NSObject <NSApplicationDelegate>
+@interface MEFAppDelegate : NSObject <NSApplicationDelegate, NSSpeechSynthesizerDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
+@property IBOutlet NSTextField *Synthesizertext;
+@property IBOutlet NSButton* speakButton;
+@property IBOutlet NSButton* BoeingMissionButton;
+@property IBOutlet NSButton* UWFightSongButton;
+@property IBOutlet NSSegmentedControl* voiceButton;
+@property NSSpeechSynthesizer *synthesizer;
+@property NSMutableArray *voiceArray;
+@property NSNumberFormatter *myNumberFormatted;
+@property IBOutlet NSTextField *outputNumber;
+@property IBOutlet NSTextField *inputNumber;
+@property IBOutlet NSMatrix *radioButtons;
+@property IBOutlet NSCell *radioWritten;
+@property IBOutlet NSCell *radioDecimal;
+@property IBOutlet NSCell *radioScience;
+@property IBOutlet NSSlider *numberSlider;
+@property IBOutlet NSTextField *Colortext;
 
 @end
--- Homework1/MEFAppDelegate.m
+++ Homework1/MEFAppDelegate.m
@@ -10,9 +10,113 @@
 
 @implementation MEFAppDelegate
 
+//- (NSSpeechSynthesizer *)applicationDidFinishLaunching:(NSNotification *)aNotification
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    self.synthesizer = [[NSSpeechSynthesizer alloc] initWithVoice: @"com.apple.speech.synthesis.voice.Fred"];
+    self.synthesizer.delegate = self;
+    self.myNumberFormatted =[[NSNumberFormatter alloc] init];
+    //self.numberSlider = [[NSSlider alloc] init];
+//    return _synthesizer;
 }
 
+- (void)speechSynthesizer:(NSSpeechSynthesizer *)sender didFinishSpeaking:(BOOL)finishedSpeaking
+{
+    //reset the buttons to their original state pg. 102 of book
+    
+    self.speakButton.enabled = YES;
+    self.voiceButton.enabled = YES;
+    self.UWFightSongButton.enabled = YES;
+    self.BoeingMissionButton.enabled = YES;
+    
+}
+
+- (IBAction)numberFieldChanged:(id)sender
+{
+    self.outputNumber.stringValue = self.inputNumber.stringValue;
+}
+
+- (IBAction)sliderChanged:(id)sender
+{
+    self.outputNumber.intValue = self.numberSlider.intValue;
+}
+
+- (IBAction)radioWritten:(id)sender
+{
+    NSLog(@"Written");
+    self.myNumberFormatted =[[NSNumberFormatter alloc] init];
+    [self.myNumberFormatted setNumberStyle:NSNumberFormatterSpellOutStyle];
+    NSNumber * myNumber = [self.myNumberFormatted:@"42"];
+    NSLog(@"%@", myNumber);
+    //self.outputNumber.stringValue = [self.myNumberFormatted stringFromNumber:[NSNumber numberWithFloat:self.outputNumber.stringValue]];
+    //NSLog(@"%@", (NSString *)[self.myNumberFormatted numberFromString:self.outputNumber.stringValue] );
+    
+}
+
+- (IBAction)radioDecimal:(id)sender
+{
+    NSLog(@"Decimal");
+    [self.myNumberFormatted setNumberStyle:NSNumberFormatterDecimalStyle];
+    self.outputNumber.stringValue = (NSString *)[self.myNumberFormatted numberFromString:self.outputNumber.stringValue];
+    NSLog(@"%@", (NSString *)[self.myNumberFormatted numberFromString:self.outputNumber.stringValue] );
+}
+
+- (IBAction)radioScience:(id)sender
+{
+    NSLog(@"Science");
+    [self.myNumberFormatted setNumberStyle:NSNumberFormatterScientificStyle];
+    self.outputNumber.stringValue = (NSString *)[self.myNumberFormatted numberFromString:self.outputNumber.stringValue];
+    NSLog(@"%@", (NSString *)[self.myNumberFormatted numberFromString:self.outputNumber.stringValue] );
+}
+
+- (IBAction)speakButtonpushed:(id)sender
+{
+    [self.synthesizer startSpeakingString:self.Synthesizertext.stringValue];
+    
+    self.speakButton.enabled = NO;
+    self.voiceButton.enabled = NO;
+    self.UWFightSongButton.enabled = NO;
+    self.BoeingMissionButton.enabled = NO;
+}
+
+
+-(IBAction)boeingMissionPressed:(id)sender
+{
+    //[[self.Synthesizertext cell] setWraps:YES];
+    //[[self.Synthesizertext cell] setLineBreakMode:NSLineBreakByWordWrapping];
+    self.Synthesizertext.stringValue = @"People working together as a global enterprise for aerospace industry leadership.";
+}
+
+-(IBAction)uwFightSongPressed:(id)sender
+{
+    //[[self.Synthesizertext cell] setWraps:YES];
+    //[[self.Synthesizertext cell] setLineBreakMode:NSLineBreakByWordWrapping];
+    self.Synthesizertext.stringValue = @"Bow Down to Washington, Bow Down to Washington. Mighty Are the Men Who Wear Purple and the Gold!";
+}
+
+-(IBAction)VoiceSelectionPressed:(id)sender
+{
+    
+    long clickedSegment = [sender selectedSegment];
+    long clickedSegmentTag = [[sender cell] tagForSegment:clickedSegment];
+    
+    if (clickedSegmentTag == 0) {
+        [_synthesizer setVoice:@"com.apple.speech.synthesis.voice.Zarvox"];
+        }
+    
+    if (clickedSegmentTag == 1) {
+        [_synthesizer setVoice:@"com.apple.speech.synthesis.voice.Kathy"];
+    }
+    
+    if (clickedSegmentTag == 2) {
+        [_synthesizer setVoice:@"com.apple.speech.synthesis.voice.Fred"];
+    }
+    
+}
+
+-(IBAction)ColorButtonPressed:(id)sender
+{
+    self.Colortext.textColor = [NSColor redColor];
+}
 @end
